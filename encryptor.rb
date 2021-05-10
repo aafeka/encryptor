a78d805ec243b354cfe09d8945c98290f7747f75
kostya <kostya@wearepush.co>
2021-05-10 22:15:53 +0300

    add SHA2, refactoring

--- encryptor.rb
+++ encryptor.rb
@@ -1,6 +1,6 @@
 require 'digest'
 
-ENCRYPTION_METHODS = %w[MD5 SHA1].freeze
+ENCRYPTION_METHODS = %w[MD5 SHA1 SHA2].freeze
 
 puts 'Введите слово или фразу для шифрования:'
 user_input = STDIN.gets.chomp
@@ -8,16 +8,21 @@ user_input = STDIN.gets.chomp
 puts 'Каким способом зашифровать:'
 ENCRYPTION_METHODS.each.with_index(1) { |method, index| puts "#{index}. #{method}" }
 
-user_option = STDIN.gets.chomp
+user_option = STDIN.gets.chomp.to_i
 
-until %w[1 2].include?(user_option)
+until (1..ENCRYPTION_METHODS.count).include?(user_option)
   puts 'Вы ввели неверное значение'
-  user_option = STDIN.gets.chomp
+  user_option = STDIN.gets.chomp.to_i
 end
 
 puts 'Вот что получилось:'
-if user_option == '1'
-  puts Digest::MD5.hexdigest user_input
-else
-  puts Digest::SHA1.hexdigest user_input
-end
+hash = case user_option
+       when 1
+         Digest::MD5.hexdigest user_input
+       when 2
+         Digest::SHA1.hexdigest user_input
+       else
+         Digest::SHA2.hexdigest user_input
+       end
+
+puts hash
